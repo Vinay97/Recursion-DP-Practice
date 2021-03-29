171475c9f3ef45b9b0d8b281a340afde385c791b
Vinay97 <vinay.yadav1910@outlook.com>
2021-03-29 19:42:39 +0530

    Soln for count of subset problem using DP.

---
+++ SubSetCount.java
@@ -0,0 +1,37 @@
+package practiceProject;
+
+public class SubSetCount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int set[] = {3, 34, 4, 12, 5, 2};
+		int sum = 12;
+		int t[][] = new int[set.length+1][sum+1];
+		
+		for(int i = 0;i<set.length;i++) {
+			for(int j = 0;j<sum+1;j++) {
+				if(i==0) {
+					t[i][j] = 0;
+				}
+				
+				if(j==0) {
+					t[i][j] = 1;
+				}
+			}
+		}
+		
+		for(int k = 1;k<set.length+1;k++) {
+			for(int l = 1;l<sum+1;l++) {
+				if(set[k-1]<=l) {
+					t[k][l] = t[k-1][l-set[k-1]]+t[k-1][l];
+				}else {
+					t[k][l] = t[k-1][l];
+				}
+			}
+		}
+		
+		System.out.println(t[set.length][sum]);
+
+	}
+
+}
